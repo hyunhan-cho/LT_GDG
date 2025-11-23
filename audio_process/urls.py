026c73349ee8b7944eb99917e570d5131f65f15f
Trashbin4943 <anthonyhwan415@gmail.com>
2025-11-23 14:18:14 +0900

    DEBUG: API 주소 - 탬플릿 매핑 오류 수정

--- audio_process/urls.py
+++ audio_process/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from .views import diarize_and_transcribe_view
+# # from django.urls import path
+# # from .views import diarize_and_transcribe_view
 
-urlpatterns = [
-#    path("diarize/", diarize_and_transcribe_view, name="diarize_and_transcribe"),
-]
+# urlpatterns = [
+# #    path("diarize/", diarize_and_transcribe_view, name="diarize_and_transcribe"),
+# ]
